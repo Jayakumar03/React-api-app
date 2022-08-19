792cf53bd25f2d782c5515d9609454363aaf0a72
maverickJAI <jaimech61@gmail.com>
2022-08-19 16:09:36 +0530

    done till city

--- README.md
+++ README.md
@@ -68,3 +68,12 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+### libiraies  used
+
+Axios : npm i axios
+bootstrap : npm i bootstrap
+reactstrap : npm i reactstrap
+react icons : npm i react-icons
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+import {Container, Row, Col  } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Axios from "axios"
 
-function App() {
+import "./App.css"
+import Card from "./Card";
+
+
+
+
+
+
+const App = () => {
+
+/* STATES FOR API AND REASON FOR OBJECT INSIDE THE USE STATE IS BECAUSE API DATA WILL BE IN JSON AND OBJECT */
+const [details, setDetails] = useState({});
+
+
+const fetchDetails = async () => {
+// this will directly  get data  from api
+  const {data} = await Axios.get("https://randomuser.me/api/") 
+  console.log("response : " ,  data)
+
+   // or
+  // const response = await Axios.get("https://randomuser.me/api/")
+  // const newData =  response.data // if we used this method then this line is extra we havs to store it in a const
+  // console.log("response : " ,  response)
+
+  const details = data.results[0]
+  // console.log(details)
+
+  setDetails(details)
+
+}
+
+  useEffect( () => {
+
+    fetchDetails()
+
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Container fluid className="p-4 bg-primary App">
+      <Row>
+        <Col md={4} className="offset-md-4 mt-4">
+          <Card details = {details}/>
+        </Col>
+      </Row>
+     </Container>
   );
+ 
 }
 
 export default App;
